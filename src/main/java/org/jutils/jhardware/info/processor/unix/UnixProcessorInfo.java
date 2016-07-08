60d556a37c437fec1910900d77e8d22e2fd862e7
profesorfalken <jgarciaalo@gmail.com>
2016-07-08 23:42:35 +0200

    Add temperature detection using jSensors

--- src/main/java/org/jutils/jhardware/info/processor/unix/UnixProcessorInfo.java
+++ src/main/java/org/jutils/jhardware/info/processor/unix/UnixProcessorInfo.java
@@ -13,8 +13,12 @@
  */
 package org.jutils.jhardware.info.processor.unix;
 
+import com.profesorfalken.jsensors.JSensors;
+import com.profesorfalken.jsensors.model.components.Cpu;
+import com.profesorfalken.jsensors.model.sensors.Temperature;
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.jutils.jhardware.info.processor.AbstractProcessorInfo;
@@ -30,40 +34,56 @@ public final class UnixProcessorInfo extends AbstractProcessorInfo {
     private static final String CPUINFO = "/proc/cpuinfo";
     private static final String CPUTEMP_THERMAL_ROOT = "/sys/class/thermal/thermal_zone";
     private static final String CPUTEMP_THERMAL_FILE = "temp";
-    
 
     public static String getProcessorData() {
         Stream<String> streamProcessorInfo = HardwareInfoUtils.readFile(CPUINFO);
         final StringBuilder buffer = new StringBuilder();
 
-        streamProcessorInfo.forEach((String line) -> 
-            buffer.append(line).append("\r\n")
+        streamProcessorInfo.forEach((String line)
+                -> buffer.append(line).append("\r\n")
         );
         return buffer.toString();
     }
 
     public static String getTemperatureData() {
         final StringBuilder buffer = new StringBuilder();
-        
-        //TODO: First try: sensors        
-        
+        int sensorIndex = 0;
+
+        //First try: jSensors
+        boolean foundByjSensors = false;
+        List<Cpu> cpus = JSensors.get.components().cpus;
+        //For the moment jHardware only handles 1 processor
+        if (cpus.size() > 0) {
+            Cpu cpu = cpus.get(0);
+            if (cpu.sensors.temperatures != null && cpu.sensors.temperatures.size() > 0) {
+                for (Temperature temp : cpu.sensors.temperatures) {
+                    if (temp.value != null) {
+                        buffer.append("cpu").append(sensorIndex++).append(":").append(temp.value.intValue()).append(";");
+                        foundByjSensors = true;
+                    }
+                }
+            }
+        }
+
         //https://www.kernel.org/doc/Documentation/thermal/sysfs-api.txt
         //Second try: /sys/class/thermal
-        int sensorIndex = 0;
-        while (true) {
-            if (new File(CPUTEMP_THERMAL_ROOT + sensorIndex).exists()) {
-                String tempFile = CPUTEMP_THERMAL_ROOT + sensorIndex + "/" + CPUTEMP_THERMAL_FILE;
-                String value = HardwareInfoUtils.getSingleValueFromFile(tempFile);
-                if (value != null && !value.isEmpty()) {
-                    int tempValue = Integer.valueOf(value.trim()) / 1000;
-                    buffer.append("cpu").append(sensorIndex).append(":").append(tempValue).append(";");
-                }                
-            } else {
-                break;
+        if (!foundByjSensors) {
+            sensorIndex = 0;
+            while (true) {
+                if (new File(CPUTEMP_THERMAL_ROOT + sensorIndex).exists()) {
+                    String tempFile = CPUTEMP_THERMAL_ROOT + sensorIndex + "/" + CPUTEMP_THERMAL_FILE;
+                    String value = HardwareInfoUtils.getSingleValueFromFile(tempFile);
+                    if (value != null && !value.isEmpty()) {
+                        int tempValue = Integer.valueOf(value.trim()) / 1000;
+                        buffer.append("cpu").append(sensorIndex).append(":").append(tempValue).append(";");
+                    }
+                } else {
+                    break;
+                }
+                sensorIndex++;
             }
-            sensorIndex++;
         }
-        
+
         return buffer.toString();
     }
 
@@ -74,8 +94,8 @@ public final class UnixProcessorInfo extends AbstractProcessorInfo {
 
         for (final String dataLine : dataStringLines) {
             String[] dataStringInfo = dataLine.split(":");
-            processorDataMap.put(dataStringInfo[0].trim(), 
-                        (dataStringInfo.length == 2) ? dataStringInfo[1].trim() : "");
+            processorDataMap.put(dataStringInfo[0].trim(),
+                    (dataStringInfo.length == 2) ? dataStringInfo[1].trim() : "");
         }
 
         String temperature = getTemperatureData();
--- src/main/java/org/jutils/jhardware/info/processor/windows/WindowsProcessorInfo.java
+++ src/main/java/org/jutils/jhardware/info/processor/windows/WindowsProcessorInfo.java
@@ -13,15 +13,19 @@
  */
 package org.jutils.jhardware.info.processor.windows;
 
+import com.profesorfalken.jsensors.JSensors;
+import com.profesorfalken.jsensors.model.components.Cpu;
+import com.profesorfalken.jsensors.model.sensors.Temperature;
 import com.profesorfalken.wmi4java.WMI4Java;
 import com.profesorfalken.wmi4java.WMIClass;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.jutils.jhardware.info.processor.AbstractProcessorInfo;
 
 /**
  * Information related to CPU
- * 
+ *
  * @author Javier Garcia Alonso
  */
 public final class WindowsProcessorInfo extends AbstractProcessorInfo {
@@ -32,8 +36,8 @@ public final class WindowsProcessorInfo extends AbstractProcessorInfo {
      */
     @Override
     protected Map<String, String> parseInfo() {
-        Map<String, String> processorDataMap = 
-                WMI4Java.get().VBSEngine().getWMIObject(WMIClass.WIN32_PROCESSOR);  
+        Map<String, String> processorDataMap
+                = WMI4Java.get().VBSEngine().getWMIObject(WMIClass.WIN32_PROCESSOR);
 
         //Line 1 CPUs infos
         String lineInfos = processorDataMap.get("Description");
@@ -46,36 +50,52 @@ public final class WindowsProcessorInfo extends AbstractProcessorInfo {
         processorDataMap.put("cpu MHz", processorDataMap.get("MaxClockSpeed"));
         processorDataMap.put("vendor_id", processorDataMap.get("Manufacturer"));
         processorDataMap.put("cpu cores", processorDataMap.get("NumberOfCores"));
-        
+
+        //Temperature
+        //First, try with jSensors
+        //First try: jSensors
+        boolean foundByjSensors = false;
+        List<Cpu> cpus = JSensors.get.components().cpus;
+        if (cpus.size() > 0) {
+            //For the moment jHardware only handles 1 processor
+            Cpu cpu = cpus.get(0);
+            if (cpu.sensors.temperatures != null && cpu.sensors.temperatures.size() > 0) {
+                for (Temperature temp : cpu.sensors.temperatures) {
+                    if (temp.value != null) {
+                        processorDataMap.put("temperature",
+                                String.valueOf(temp.value.intValue()));
+                        foundByjSensors = true;
+                    }
+                }
+            }
+        }
+
         //First try with Win32_TemperatureProbe 
         //TODO: should be nice but it is not supported
-        
         //https://msdn.microsoft.com/en-us/library/aa394493(v=vs.85).aspx
-        
         //Most of the information that the Win32_TemperatureProbe WMI class provides
         //comes from SMBIOS. Real-time readings for the CurrentReading property 
         //cannot be extracted from SMBIOS tables. For this reason, 
         //current implementations of WMI do not populate the CurrentReading property. 
         //The CurrentReading property's presence is reserved for future use.
         /*Map<String, String> temperatureProbeDataMap = 
-                WMI4Java.get().VBSEngine().getWMIObject("Win32_TemperatureProbe");*/
-        
-        
+         WMI4Java.get().VBSEngine().getWMIObject("Win32_TemperatureProbe");*/
         //We take the temperature from MSAcpi_ThermalZoneTemperature
         //This is not optimal since most manufacturers does not fill this information.
         //Moreover, the values are not updated in real time.
-        
         //It does not even give a temperature separated by core. I have to improve this somehow...
-        Map<String, String> temperatureDataMap = 
-                WMI4Java.get().VBSEngine().namespace("root/wmi").getWMIObject("MSAcpi_ThermalZoneTemperature");
-        
-        if (temperatureDataMap.containsKey("CurrentTemperature")) {
-            //Convert from tens of kelvin to centigrades
-            processorDataMap.put("temperature", 
-                    String.valueOf(Integer.valueOf(processorDataMap.get("CurrentTemperature")) / 10 - 273));
-        } else {
-            processorDataMap.put("Temperature", 
-                    "NOT DETECTED");
+        if (!foundByjSensors) {
+            Map<String, String> temperatureDataMap
+                    = WMI4Java.get().VBSEngine().namespace("root/wmi").getWMIObject("MSAcpi_ThermalZoneTemperature");
+
+            if (temperatureDataMap.containsKey("CurrentTemperature")) {
+                //Convert from tens of kelvin to centigrades
+                processorDataMap.put("temperature",
+                        String.valueOf(Integer.valueOf(processorDataMap.get("CurrentTemperature")) / 10 - 273));
+            } else {
+                processorDataMap.put("Temperature",
+                        "NOT DETECTED");
+            }
         }
 
         return processorDataMap;
