1b3ad7b3f0527a9a89ea16b280cfb45d095728b5
profesorfalken <jgarciaalo@gmail.com>
2016-07-09 15:10:37 +0200

    Starts graphic cards implementation

---
+++ src/main/java/org/jutils/jhardware/info/graphicscard/AbstractGraphicsCardInfo.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 javier.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jutils.jhardware.info.graphicscard;
+
+import java.util.Map;
+import org.jutils.jhardware.info.HardwareInfo;
+import org.jutils.jhardware.model.ProcessorInfo;
+
+/**
+ *
+ * @author Javier Garcia Alonso.
+ */
+public abstract class AbstractGraphicsCardInfo implements HardwareInfo {
+
+    @Override
+    public ProcessorInfo getInfo() {
+        return buildFromDataMap(parseInfo());
+    }
+    
+    protected abstract Map<String, String> parseInfo();
+    
+    protected ProcessorInfo buildFromDataMap(Map<String, String> dataMap) {
+        ProcessorInfo info = new ProcessorInfo();
+        info.setFullInfo(dataMap);
+        
+        if (dataMap != null && !dataMap.isEmpty()) {
+            info.setCacheSize(dataMap.get("cache size"));
+            info.setFamily(dataMap.get("cpu family"));
+            info.setMhz(dataMap.get("cpu MHz"));
+            info.setModel(dataMap.get("model"));
+            info.setModelName(dataMap.get("model name"));
+            info.setNumCores(dataMap.get("cpu cores"));
+            info.setStepping(dataMap.get("stepping"));
+            info.setTemperature(dataMap.get("temperature"));
+            info.setVendorId(dataMap.get("vendor_id"));
+        }
+        
+        return info;
+    }
+}
---
+++ src/main/java/org/jutils/jhardware/model/GraphicsCardInfo.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2016 Javier Garcia Alonso.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jutils.jhardware.model;
+
+/**
+ * Model that encapsulates graphics card information
+ * 
+ * @author Javier Garcia Alonso
+ */
+public class GraphicsCardInfo implements ComponentInfo{
+    private String name;
+    private String manufacturer;
+    private String dacType;
+    private String deviceType;
+    private String temperature;
+    private String fanSpeed;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getDacType() {
+        return dacType;
+    }
+
+    public void setDacType(String dacType) {
+        this.dacType = dacType;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getFanSpeed() {
+        return fanSpeed;
+    }
+
+    public void setFanSpeed(String fanSpeed) {
+        this.fanSpeed = fanSpeed;
+    }
+    
+}
